5a68e77818ba4d6ea9d3f33de3aad79e60491a1c
Lorenzo Bianchi <lorenzo.bianchi@confidence.com>
2023-10-30 15:45:05 +0100

    styling footer and navbar

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,21 +1,28 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { FaFacebookSquare, FaInstagramSquare } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="bg-blue-50 border-t border-blue-200">
       <Container>
         <div className="py-20 flex flex-col lg:flex-row lg:justify-between items-center">
-          <div className='sm:text-center'>
-            <h6 className='text-blue-600'>SCI CLUB CHIARI</h6>
+          <div className='text-center'>
+            <h6 className='text-blue-600 font-semibold'>SCI CLUB CHIARI</h6>
             <span>Via San Bernardino, 8</span><br/>
             <span>25032 Chiari (BS)</span>
           </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <div className='sm:mt-5 sm:text-center'>
+            <div className='mt-5 text-center'>
               <p className='m-0'><b>Telefono</b> +39 366 300 58 02</p>
-              <p className='m-0'><b>Facebook</b>	<a href='https://it-it.facebook.com/sciclubchiari.it/' target='blank'>https://it-it.facebook.com/sciclubchiari.it/</a></p>
-              <p className='m-0'><b>E-mail</b> <a href='mailto:info@sciclubchiari.it' target='blank'>info@sciclubchiari.it</a></p>
+              <p className='m-0'><b>E-mail</b> <a href='mailto:info@sciclubchiari.it' className='hover:underline hover:text-blue-400' target='blank'>info@sciclubchiari.it</a></p>
+              <div className="flex flex-row mt-2 items-center justify-center">
+                <a href='https://it-it.facebook.com/sciclubchiari.it/' target='blank'>
+                  <FaFacebookSquare className='text-3xl text-blue-600 hover:text-blue-400' />
+                </a>
+                <a href='https://www.instagram.com/sci_club_chiari_asd/' target='blank'>
+                  <FaInstagramSquare className='text-3xl text-blue-600 ml-2 hover:text-blue-400' />
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,67 +1,89 @@
 import Link from "next/link";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toogleMobileMunu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <div className="w-full sticky top-0 z-50 text-gray-700 bg-white" style={{ zIndex: 10000 }}>
-      <div className="flex flex-col px-2 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
-        <div className="p-4 flex flex-row items-center justify-between">
-          <Link
-            href="/"
-            className="flex title-font font-medium items-center text-gray-900"
-          >
-            <img src="/assets/ski-64.png" alt="ski icon" />
-            <span className="ml-3 font-bold text-xl">
-              Sci Club Chiari
-            </span>
-          </Link>
-          <div className="mt-1 ml-4">
-            {/* <button
-              type="button"
-              aria-label="dark-mode-toggle"
-              className="text-emerald-500 flex justify-center p-2 text-gray-500 transition duration-150 ease-in-out bg-gray-100 border border-transparent rounded-md lg:bg-white  dark:bg-slate-800   focus:outline-none bg-gray-50 dark:bg-slate-700 "
+    <>
+      <div
+        className="w-full sticky top-0 text-gray-700 bg-white"
+        style={{ zIndex: 10000 }}
+      >
+        <div className="flex flex-col px-2 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+          <div className="p-4 flex flex-row items-center justify-between">
+            <Link
+              href="/"
+              className="flex title-font font-medium items-center text-gray-900 hover:text-blue-900"
             >
-              <span>
-                <svg
-                  className="w-6 h-6 text-yellow-500"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </span>
-            </button> */}
+              <img src="/assets/ski-64.png" alt="ski icon" />
+              <span className="ml-3 font-bold text-xl">Sci Club Chiari</span>
+            </Link>
+            <button
+              aria-label="mobile-menu"
+              onClick={toogleMobileMunu}
+              className="md:hidden rounded-lg focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
+            >
+              {isMobileMenuOpen ? <FaTimes className="w-6 h-6 text-gray-900 hover:text-blue-900" /> : <FaBars className="w-6 h-6 text-gray-900 hover:text-blue-900" />}
+            </button>
           </div>
-          <button
-            aria-label="mobile-menu"
-            className="md:hidden rounded-lg focus:outline-none focus:shadow-outline"
-          ></button>
+          <nav
+            className={`flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row`}
+          >
+            <Link className="md:mt-0 mt-3 px-3 py-2" href="/sci-club">
+              Sci club
+            </Link>
+            <Link className="md:mt-0 mt-3 px-3 py-2" href="/attivita">
+              Attività
+            </Link>
+            <Link className="md:mt-0 mt-3 px-3 py-2" href="/tesseramento">
+              Tesseramento
+            </Link>
+            <Link className="md:mt-0 mt-3 px-3 py-2" href="/news">
+              News
+            </Link>
+            <Link className="md:mt-0 mt-3 px-3 py-2" href="/contatti">
+              Contatti
+            </Link>
+          </nav>
         </div>
-        <nav
-          className={`flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row`}
-        >
-          <Link className="md:mt-0 mt-3 px-3 py-2" href="/sci-club">
-            Sci club
-          </Link>
-          <Link className="md:mt-0 mt-3 px-3 py-2" href="/attivita">
-            Attività
-          </Link>
-          <Link className="md:mt-0 mt-3 px-3 py-2" href="/tesseramento">
-            Tesseramento
-          </Link>
-          <Link className="md:mt-0 mt-3 px-3 py-2" href="/news">
-            News
-          </Link>
-          <Link className="md:mt-0 mt-3 px-3 py-2" href="/contatti">
-            Contatti
-          </Link>
-        </nav>
       </div>
-    </div>
+      {isMobileMenuOpen && (
+        <>
+          <div
+            className="fixed w-full transition-opacity ease-linear duration-300 bg-white opacity-100 flex flex-col justify-center items-center py-5"
+            onClick={() => setIsMobileMenuOpen(false)}
+            style={{ zIndex: 10000 }}
+          >
+            <Link className="px-3 py-2" href="/sci-club">
+              Sci club
+            </Link>
+            <Link className="px-3 py-2" href="/attivita">
+              Attività
+            </Link>
+            <Link className="px-3 py-2" href="/tesseramento">
+              Tesseramento
+            </Link>
+            <Link className="px-3 py-2" href="/news">
+              News
+            </Link>
+            <Link className="px-3 py-2" href="/contatti">
+              Contatti
+            </Link>
+          </div>
+          <div
+            className="fixed inset-0 bg-black opacity-50 z-40"
+            onClick={() => setIsMobileMenuOpen(false)}
+            style={{ zIndex: 1500 }}
+          />
+        </>
+      )}
+    </>
   );
 };
 
