34162cb301a3f6b0ee2506d8765db88fa05c88cd
loreBianchi <lorebianchi123@gmail.com>
2025-02-23 14:48:11 +0100

    docs(sci-club): modified statuto and added safegaurding

--- pages/sci-club/sci-club-content.tsx
+++ pages/sci-club/sci-club-content.tsx
@@ -6,7 +6,7 @@ export default function SciClubContent() {
           Statuto
         </h2>
         <p className="mt-1 text-base leading-7 text-slate-700">
-          Scarica lo statuto del nostro Sci Club <a href="/assets/docs/atto-costitutivo-Sci-Club-Chiari-Asd.pdf" className="text-blue-600 hover:underline" target="_blank">Statuto</a>
+          Scarica lo statuto del nostro Sci Club <a href="/assets/docs/statuto.pdf" className="text-blue-600 hover:underline" target="_blank">statuto</a>
         </p>
       </div>
       <div className="mt-5 mb-20">
@@ -14,7 +14,15 @@ export default function SciClubContent() {
           Regolamento
         </h2>
         <p className="mt-1 text-base leading-7 text-slate-700">
-          Clicca qui per scaricare il regolamento <a href="/assets/docs/Regolamento.pdf" className="text-blue-600 hover:underline" target="_blank">Regolamento</a>
+          Clicca il link per scaricare il regolamento <a href="/assets/docs/Regolamento.pdf" className="text-blue-600 hover:underline" target="_blank">regolamento</a>
+        </p>
+      </div>
+      <div className="mt-5 mb-20">
+        <h2 className="mt-2 text-lg font-medium text-slate-900">
+          Safeguarding
+        </h2>
+        <p className="mt-1 text-base leading-7 text-slate-700">
+          Clicca il link per scaricare l'<a href="/assets/docs/safeguarding.pdf" className="text-blue-600 hover:underline" target="_blank">informativa</a>
         </p>
       </div>
       <div className="mt-5 mb-20">
